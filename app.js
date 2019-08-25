d3be6f75f5f03a07adf50a9c6c6a58881396ccb1
hunter-s70 <serg.buhtoyarov@gmail.com>
2019-08-25 22:22:57 +0300

    add body parser and products test form

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // middleware
-app.use((req, res, next) => {
-  console.log('log');
+app.use(bodyParser.urlencoded({extend: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(`
+    <h1>Add a product</h1>
+    <form action="/product" method="post">
+      <input type="text" name="title" />
+      <button type="submit">Add product</button>
+    </form>
+`);
 });
 
-const server = http.createServer(app);
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Page not found</h1>');
+});
+
+app.listen(3000);
