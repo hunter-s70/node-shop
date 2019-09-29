0002ef4d27708472c485a32032f35c8737e98ffb
hunter-s70 <serg.buhtoyarov@gmail.com>
2019-09-29 12:23:10 +0300

    add products controller

--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const adminData = require('./admin');
+const ProductController = require('../controller/products');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', ProductController.getProductsList);
 
 module.exports = router;
