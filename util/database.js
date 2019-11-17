8df20cb688892ec2f5f108d7fbb19e90fabdcfa8
hunter-s70 <serg.buhtoyarov@gmail.com>
2019-11-17 11:54:51 +0200

    add dotenv for config data

--- util/database.js
+++ util/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const pool = mysql.createPool({
-  host: '',
-  user: '',
-  database: '',
-  password: ''
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS
 });
 
 module.exports = pool.promise();
