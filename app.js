4caa26e70acf7c988b721e6794cde703baceb03d
hunter-s70 <serg.buhtoyarov@gmail.com>
2019-11-17 11:09:33 +0200

    add mysql connection

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const ErrorsController = require('./controller/errors');
 
+const db = require('./util/database');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+db.execute('SELECT * FROM products').then(result => {
+  console.log(result[0]);
+}).catch(err => {
+  console.log(err);
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
