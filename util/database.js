3cb9868410338c08543dd0fd162dfbdc351a123a
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-01-06 15:23:39 +0200

    rename db_name env variable for sequelize

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // create the connection to database
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
+  process.env.DB_NAME_SQLIZE,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
