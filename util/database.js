e4c30df6b871a7934372c757b0d1b47609dcd735
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-01-06 15:22:58 +0200

    rename db_name env variable

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME_SQL,
   password: process.env.DB_PASS
 });
 
