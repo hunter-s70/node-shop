c65422ad1b163a5b961e7d1eca9b1ea490293d67
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-03-07 17:06:35 +0200

    install mongoose

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const ErrorsController = require('./controller/errors');
+const mongoose = require('mongoose');
 
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -19,23 +19,23 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({extend: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5e3079d196b06e1eb012a51a').then(user => {
-    req.user = new User({
-      id: user._id,
-      username: user.name,
-      email: user.email,
-      cart: user.cart
-    });
-    next();
-  }).catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5e3079d196b06e1eb012a51a').then(user => {
+//     req.user = new User({
+//       id: user._id,
+//       username: user.name,
+//       email: user.email,
+//       cart: user.cart
+//     });
+//     next();
+//   }).catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(ErrorsController.get404);
 
-mongoConnect(() => {
+mongoose.connect(`${process.env.DB_URL_MONGO}/${process.env.DB_NAME_MONGO}`).then(() => {
   app.listen(3000);
-});
+}).catch(err => console.log(err));
