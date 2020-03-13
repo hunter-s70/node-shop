a9c383728563a565eca32300881a4a52ac4d849c
hunter-s70 <serg.buhtoyarov@gmail.com>
2020-03-13 23:09:09 +0200

    add relations to user

--- controller/admin.js
+++ controller/admin.js
@@ -13,8 +13,8 @@ exports.postProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  // const userId = req.user._id;
-  const product = new Product({title, imageUrl, description, price});
+  const userId = req.user._id;
+  const product = new Product({title, imageUrl, description, price, userId});
   product.save()
     .then(() => {
       res.redirect('/admin/products');
